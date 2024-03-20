06e4b261ab9eca302783e36e59303d86d8378ff2
Hadja Aïssatou BALDE <151042235+Hkdjaa@users.noreply.github.com>
2024-03-20 21:01:45 +0000

    Update pageconnect.php

--- php/pageconnect.php
+++ php/pageconnect.php
@@ -41,7 +41,7 @@ if(isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
-    <link href="../css/pageconnect.css" rel="stylesheet" />
+    <link href="css/pageconnect.css" rel="stylesheet" />
     <title>Page de connexion</title>
 </head>
 <body>
